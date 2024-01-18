ec0586f46837f7c962e8ec75501433939450b885
nguoidepxuhue <ngocungducanh2022@gmail.com>
2024-01-19 01:14:21 +0700

    them nut xoa post

--- src/components/Home/cpnPost/PostHeader.js
+++ src/components/Home/cpnPost/PostHeader.js
@@ -10,13 +10,12 @@ import {
 import highLight from "./../../../styles/highLightStyles";
 import Modal from "react-native-modal";
 import headerPostStyles from "./../../../styles/postHeaderStyles";
-import {
-  heightPercentageToDP,
-  widthPercentageToDP,
-} from "react-native-responsive-screen";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const PostHeader = ({ post, onAvatarPress, onEllipsisPress, users }) => {
   const { username, avt } = post;
+
+  const [isModalVisible, setModalVisible] = useState(false);
 
   const handleAvatarPress = () => {
     console.log("Avatar pressed!");
@@ -24,25 +23,59 @@ const PostHeader = ({ post, onAvatarPress, onEllipsisPress, users }) => {
       onAvatarPress();
     }
   };
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const handleDeletePost = () => {
+    // Đặt logic xóa bài post ở đây
+    console.log("Bài post đã bị xóa!");
+    // Sau khi xóa xong, đóng modal
+    toggleModal();
+  };
+
   return (
     <View style={headerPostStyles.containerHeaderPost}>
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
         <TouchableHighlight
           style={highLight.highLightAVTpost}
           underlayColor="lightgray"
           onPress={handleAvatarPress}
         >
-        {users[post.ownerUserId].detail.avatarUrl ? (
-          <Image style={headerPostStyles.avatar} source={{uri: users[post.ownerUserId].detail.avatarUrl}} />
-        ): (
-          <Image style={headerPostStyles.avatar}/>
-        )}
-
+          {users[post.ownerUserId].detail.avatarUrl ? (
+            <Image
+              style={headerPostStyles.avatar}
+              source={{ uri: users[post.ownerUserId].detail.avatarUrl }}
+            />
+          ) : (
+            <Image style={headerPostStyles.avatar} />
+          )}
         </TouchableHighlight>
-        <View>
-          <Text style={headerPostStyles.userName}>{users[post.ownerUserId].detail.name}</Text>
+        <View style={{ flex: 3 }}>
+          <Text style={headerPostStyles.userName}>
+            {users[post.ownerUserId].detail.name}
+          </Text>
         </View>
+        <TouchableOpacity style={{ paddingHorizontal: 20 }} onPress={toggleModal}>
+          <MaterialCommunityIcons name="dots-horizontal" size={25} />
+        </TouchableOpacity>
       </View>
+
+      {/* Modal */}
+      <Modal
+        isVisible={isModalVisible}
+        onBackdropPress={toggleModal}
+        style={{ justifyContent: 'flex-end', margin: 0 }}
+      >
+        <View style={{ backgroundColor: 'white', padding: 16, height: 70 }}>
+          <TouchableOpacity onPress={handleDeletePost}>
+            <Text style={{ color: 'red', textAlign: 'center', fontSize: 20 }}>
+              Delete post
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
