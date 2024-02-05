acfbf299e2ff0466edcedc0e55fde9a836240775
AzenKain <doduykhanh344@gmail.com>
2024-02-05 14:47:53 +0700

    fix roomchat

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, View } from "react-native";
-import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useEffect, useState } from "react";
